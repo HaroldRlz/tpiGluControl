db9425a734f53b08542850b412be1101bd91200b
Harold <hsrodriguezl@unal.edu.co>
2018-04-14 21:03:58 -0500

    Version base para la App

--- app/src/main/java/com/tpigrupo30/glucontrol/MainActivity.java
+++ app/src/main/java/com/tpigrupo30/glucontrol/MainActivity.java
@@ -1,7 +1,14 @@
 package com.tpigrupo30.glucontrol;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
+import com.tpigrupo30.glucontrol.View.fragment.GraphicsFragment;
+import com.tpigrupo30.glucontrol.View.fragment.HomeFragment;
+import com.tpigrupo30.glucontrol.View.fragment.NutritionFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottombar);
+        bottomBar.setDefaultTab(R.id.home);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(int tabId) {
+                switch (tabId){
+                    case R.id.home:
+                        HomeFragment homeFragment = new HomeFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
+                        break;
+                    case R.id.graphics:
+                        GraphicsFragment graphicsFragment = new GraphicsFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,graphicsFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
+                        break;
+                    case R.id.nutrition:
+                        NutritionFragment nutritionFragment = new NutritionFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,nutritionFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
+                        break;
+                }
+            }
+        });
     }
 }
